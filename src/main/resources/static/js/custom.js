8eb2118cb063cc1cfaaee8ed47b5b7a53e46e41f
tekit <tekityt@gmail.com>
2023-03-23 19:59:39 +0200

    main design

---
+++ src/main/resources/static/js/custom.js
@@ -0,0 +1,7 @@
+$(document).ready(function () {
+    $(".main").onepage_scroll({
+        sectionContainer: "section",
+        responsiveFallback: 200,
+        loop: true
+    });
+});
---
+++ src/main/resources/static/js/typing.js
@@ -0,0 +1,9 @@
+const auto = new Typed('.auto-text', {
+    strings: ["Hi! Welcome to the best clicker site!", "There you can... play and beat records!","Scroll down to see more of somthn"],
+    loop: true,
+    IPColor: "white",
+    typeSpeed: 0.7,
+    backSpeed: 0.7
+});
+
+auto.start();
