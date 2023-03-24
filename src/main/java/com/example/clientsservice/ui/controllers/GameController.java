14bc7852720292634518aa6273040cedc1bbe2ad
tekit <tekityt@gmail.com>
2023-03-24 20:49:11 +0200

    top fix

--- src/main/java/com/example/clientsservice/ui/controllers/GameController.java
+++ src/main/java/com/example/clientsservice/ui/controllers/GameController.java
@@ -11,10 +11,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import java.util.HashMap;
-import java.util.Iterator;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 import static com.example.clientsservice.devdep.Logger.printInFixColor;
 
@@ -26,29 +23,47 @@ public class GameController {
 
     @Autowired
     UserGameService ugService;
+
     @GetMapping("/games")
     public String main(Model model) {
         model.addAttribute("games", gameService.findAll());
         return "game/main";
     }
+
     @GetMapping("/games/game")
     public String game(Model model, @RequestParam("id") Integer id) {
         model.addAttribute("game", gameService.findById(id));
         return "game/gamePattern";
     }
+
     @GetMapping("/games/gameRecords")
     public String gameRecords(Model model, @RequestParam("id") Integer id) {
         Map<User, Integer> userRecords = new HashMap<>();
         List<User> users = ugService.findUsersByGameId(id);
         List<UserGame> records = ugService.findByGameId(id);
+        List<Integer> recordsInt = new ArrayList<>();
 
+        for (int i = 0; i < records.size(); i++) {
+            recordsInt.add(records.get(i).getRating());
+        }
+
+        recordsInt.sort(Comparator.reverseOrder());
         Iterator<User> userIterator = users.iterator();
         for (int i = 0; i < records.size() && userIterator.hasNext(); i++) {
             User user = userIterator.next();
-            userRecords.put(user, records.get(i).getRating());
+            userRecords.put(user, recordsInt.get(i));
         }
-        printInFixColor(userRecords);
-        model.addAttribute("users", userRecords);
+        LinkedHashMap<User, Integer> sortedUsersRecordsMap = new LinkedHashMap<>();
+        for (int num : recordsInt) {
+            for (Map.Entry<User, Integer> entry : userRecords.entrySet()) {
+                if (entry.getValue().equals(num)) {
+                    sortedUsersRecordsMap.put(entry.getKey(), num);
+                }
+            }
+        }
+
+        printInFixColor(sortedUsersRecordsMap);
+        model.addAttribute("users", sortedUsersRecordsMap);
         return "game/topTable";
     }
 }
